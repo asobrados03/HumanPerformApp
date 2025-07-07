52ec315c2a600d75121d87bf2a233e74f1333341
Alfredo Sobrados <alfredo.118s347@gmail.com>
2025-07-07 18:30:32 +0200

    refactor: some little refactors

--- androidApp/src/main/java/com/humanperformcenter/ui/viewmodel/state/CoachState.kt
+++ androidApp/src/main/java/com/humanperformcenter/ui/viewmodel/state/CoachState.kt
@@ -5,6 +5,6 @@ import com.humanperformcenter.shared.data.model.Professional
 sealed class CoachState {
     object Idle : CoachState()
     object Loading : CoachState()
-    data class Success(val blogs: List<Professional>) : CoachState()
+    data class Success(val coaches: List<Professional>) : CoachState()
     data class Error(val message: String) : CoachState()
 }
