4f416a66c2a42dd97451eb41947b3e29e989d88f
Dimitar Vasilev Vasilev <146078645+Miti33@users.noreply.github.com>
2025-08-18 15:31:29 +0200

    fix

--- shared/src/commonMain/kotlin/com/humanperformcenter/shared/domain/repository/PaymentRepository.kt
+++ shared/src/commonMain/kotlin/com/humanperformcenter/shared/domain/repository/PaymentRepository.kt
@@ -6,8 +6,7 @@ import com.humanperformcenter.shared.data.model.RebillRequest
 interface PaymentRepository {
     suspend fun generatePaymentUrl(request: PaymentRequest) : String
     suspend fun requestGooglePay(requestJson: String): String
-    suspend fun sendTokenToBackend(token: String, amount: Int, currency: String): Boolean}
+    suspend fun sendTokenToBackend(token: String, amount: Int, currency: String): Boolean
     suspend fun getPaymentMethod(user_id: Int): String?
     suspend fun rebillPayment(rebillRequest: RebillRequest): Boolean
-
 }
