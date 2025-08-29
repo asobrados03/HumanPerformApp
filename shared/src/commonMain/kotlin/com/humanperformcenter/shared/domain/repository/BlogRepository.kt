bf4a8546e107f748a11b18dbcdb0e3c2f6e475a7
Alfredo Sobrados <alfredo.118s347@gmail.com>
2025-08-29 12:13:28 +0200

    a little refactor

--- shared/src/commonMain/kotlin/com/humanperformcenter/shared/domain/repository/BlogRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.humanperformcenter.shared.domain.repository
-
-import com.humanperformcenter.shared.data.model.BlogEntry
-
-interface BlogRepository {
-    suspend fun readBlogs(): Result<List<BlogEntry>>
-}
