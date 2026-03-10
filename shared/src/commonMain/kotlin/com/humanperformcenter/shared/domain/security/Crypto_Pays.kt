f82148c171ade356e72458f4bf38a9eaeac5ce39
Alfredo Sobrados <alfredo.118s347@gmail.com>
2026-03-10 20:08:34 +0100

    a little change

--- shared/src/commonMain/kotlin/com/humanperformcenter/shared/domain/security/Crypto_Pays.kt
+++
@@ -1,7 +0,0 @@
-package com.humanperformcenter.shared.domain.security
-import com.humanperformcenter.shared.data.model.EncryptedResult
-
-expect object Crypto_Pays {
-    fun sha256(input: String): String
-    fun encryptAES(input: String, key: String): EncryptedResult
-}
