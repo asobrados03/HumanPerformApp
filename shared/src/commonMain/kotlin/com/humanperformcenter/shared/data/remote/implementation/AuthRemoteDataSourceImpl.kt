a7bba593eecf2107488092a3a97d8e7da71779a7
Alfredo Sobrados González <alfredo.118s347@gmail.com>
2026-04-15 19:53:59 +0100

    Fix auth error mapping and register test DOB fixture

--- shared/src/commonMain/kotlin/com/humanperformcenter/shared/data/remote/implementation/AuthRemoteDataSourceImpl.kt
+++ shared/src/commonMain/kotlin/com/humanperformcenter/shared/data/remote/implementation/AuthRemoteDataSourceImpl.kt
@@ -8,7 +8,6 @@ import com.humanperformcenter.shared.data.model.auth.RegisterResponse
 import com.humanperformcenter.shared.data.model.auth.ResetPasswordRequest
 import com.humanperformcenter.shared.data.network.HttpClientProvider
 import com.humanperformcenter.shared.data.remote.AuthRemoteDataSource
-import com.humanperformcenter.shared.domain.DomainException
 import io.ktor.client.call.body
 import io.ktor.client.plugins.expectSuccess
 import io.ktor.client.request.bearerAuth
@@ -40,7 +39,7 @@ class AuthRemoteDataSourceImpl(
         }
 
         if (response.status != HttpStatusCode.OK) {
-            throw DomainException.BadRequest(details = response.errorMessageOrFallback())
+            error("HTTP ${response.status.value}: ${response.errorMessageOrFallback()}")
         }
         response.body<LoginResponse>()
     }
