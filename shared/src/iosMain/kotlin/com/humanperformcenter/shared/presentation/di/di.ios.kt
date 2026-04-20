c1943b2f2cbe3b8d7eec307fe904e9b6986e7040
Alfredo Sobrados <alfredo.118s347@gmail.com>
2026-04-21 00:01:02 +0200

    a little refactor

--- shared/src/iosMain/kotlin/com/humanperformcenter/shared/presentation/di/di.ios.kt
+++ shared/src/iosMain/kotlin/com/humanperformcenter/shared/presentation/di/di.ios.kt
@@ -1,12 +1,17 @@
 package com.humanperformcenter.shared.presentation.di
 
 import com.humanperformcenter.shared.SessionNotificationManager
+import com.humanperformcenter.shared.data.local.AuthLocalDataSource
 import com.humanperformcenter.shared.domain.storage.DataStoreProvider
+import com.humanperformcenter.shared.domain.storage.SecureStorage
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
 actual val platformModule: Module = module {
     single { DataStoreProvider.get() }
+    single<AuthLocalDataSource> {
+        SecureStorage.also { it.initialize(get()) }  // get() resuelve el DataStore de arriba
+    }
     single<SessionNotificationManager> { IOSSessionNotificationManager() }
 }
 
