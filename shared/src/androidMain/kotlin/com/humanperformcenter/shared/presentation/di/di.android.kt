55602a1f62bb6b30ef133513df3e9140b0c30509
Alfredo Sobrados González <alfredo.118s347@gmail.com>
2026-04-21 10:26:48 +0100

    Unify DataStore creation under Koin across platforms

--- shared/src/androidMain/kotlin/com/humanperformcenter/shared/presentation/di/di.android.kt
+++ shared/src/androidMain/kotlin/com/humanperformcenter/shared/presentation/di/di.android.kt
@@ -1,12 +1,20 @@
 package com.humanperformcenter.shared.presentation.di
 
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
 import com.humanperformcenter.shared.AndroidSessionNotificationManager
 import com.humanperformcenter.shared.SessionNotificationManager
-import com.humanperformcenter.shared.domain.storage.DataStoreProvider
+import com.humanperformcenter.shared.domain.storage.DATA_STORE_FILE_NAME
+import com.humanperformcenter.shared.domain.storage.createDataStore
+import org.koin.android.ext.koin.androidContext
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
 actual val platformModule: Module = module {
-    single { DataStoreProvider.get() }
+    single<DataStore<Preferences>> {
+        createDataStore {
+            androidContext().filesDir.resolve(DATA_STORE_FILE_NAME).absolutePath
+        }
+    }
     single<SessionNotificationManager> { AndroidSessionNotificationManager(get()) }
 }
